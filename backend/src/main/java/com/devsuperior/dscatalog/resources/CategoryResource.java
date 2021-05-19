c250e571dc3655c12b8779fc7a90afd50d43bc09
Vantercarlos <carloseasy@hotmail.com>
2021-05-19 00:43:20 -0400

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -35,6 +35,11 @@ public class CategoryResource {
         dto = categoryService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
-        
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+        dto = categoryService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
     }
 }
